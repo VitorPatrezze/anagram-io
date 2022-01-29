93ea4b01bcfea068d97897853e0958930869198d
Vitor Patrezze Nunes <vitorpatrezzen@gmail.com>
2022-01-29 18:28:23 -0300

    finished Fronted

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,14 @@
 import { FaTimes } from 'react-icons/fa';
 
-const Task = ({ task, onDelete }) => {
-    return <div className='task' >
+const Task = ({ task, onDelete, onToggle }) => {
+    return <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
         <h3>
             {task.text}
             <FaTimes style={{ borderRadius: '3px', backgroundColor: 'red', color: 'white', cursor: 'pointer' }}
                 onClick={onDelete} />
         </h3>
         <p>{task.day}</p>
+
     </div>;
 };
 
